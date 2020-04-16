cb9f95d9169912472e9bc5a36df9813ec6018b24
Pepe Párraga <31965296+PepeParraga@users.noreply.github.com>
2020-04-16 16:33:19 -0500

    Proyecto Librería

---
+++ books/books/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.db.models import Q # new
+from django.views.generic import ListView, DeleteView, DetailView
+from .models import Book
+
+class BookListView(LoginRequiredMixin, ListView):
+    model = Book
+    context_object_name = 'book_list' # new
+
+    template_name = 'books/book_list.html'
+    login_url = 'account_login' # new
+
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    model = Book
+    context_object_name = 'book' # new
+
+    template_name = 'books/book_detail.html'
+    login_url = 'account_login' # new
+
+    permission_required = 'books.special_status' # new
+
+class SearchResultsListView(ListView): # new
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        return Book.objects.filter(  Q(title__icontains=query) | Q(author__icontains=query) )
---
+++ books/pages/tests.py
@@ -0,0 +1,26 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+from .views import HomePageView
+
+class HomepageTests(SimpleTestCase):
+        
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self): # new
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self): # new
+        response = self.client.get('/')
+        self.assertContains(response, 'Página Principal del Proyecto Libros')
+
+    def test_homepage_does_not_contain_incorrect_html(self): # new
+        response = self.client.get('/')
+        self.assertNotContains(response, 'Hi there! I should not be on the page.')
